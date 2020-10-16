5b948700d8a25b62ab0a09c8412fd9401507ec84
Fabio Medeiros <trabalhofm@gmail.com>
2020-10-16 13:13:03 -0300

    Criação da classe de tipos primitivos

--- Source/TiposPrimitivos.java
+++ Source/TiposPrimitivos.java
@@ -18,18 +18,18 @@ public class TiposPrimitivos {
 
 	public static void main(String[] args){
 
-		char caracter = 'F'; 	// Armazena um caracter unicode, seu valor minimo '\u0000' (0) e o seu valor maximo '\uffff' (65535).
-		boolean logico = true; 	// Armazena um valor logico true ou false, por padrao e false.
+		char caracter = 'F'; 	// Armazena um caracter unicode, seu valor mínimo '\u0000' (0) e o seu valor máximo '\uffff' (65535).
+		boolean logico = true; 	// Armazena um valor logico true ou false, por padrão é false.
 
-		// Tipos primitivos numericos.
-		byte numeroInteiro8b = 1; 	// Armazena um numero inteiro 2^8 (-128 a 127) ou 8 bits, por padrao e 0.
-		short numeroInteiro16b= 55; 	// Armazena um numero inteiro 2^16 (-32.768 a 32.767) ou 16 bits, por padrao e 0.
-		int numeroInteiro32b = 541;	// Armazena um numero inteiro 2^32 (-2147483648 a 2147483647) ou 32 bits, por padrao e 0.
-		long numeroInteiro64b = 8l;	// Armazena um numero inteiro 2^64 ou 64 bits, por padrao e 0l.
+		// Tipos primitivos numéricos.
+		byte numeroInteiro8b = 1; 	// Armazena um número inteiro 2^8 (-128 a 127) ou 8 bits, por padrão é 0.
+		short numeroInteiro16b= 55; 	// Armazena um número inteiro 2^16 (-32.768 a 32.767) ou 16 bits, por padrão é 0.
+		int numeroInteiro32b = 541;	// Armazena um número inteiro 2^32 (-2147483648 a 2147483647) ou 32 bits, por padrão é 0.
+		long numeroInteiro64b = 8l;	// Armazena um número inteiro 2^64 ou 64 bits, por padrão é 0l.
 
-		// Tipos primitivos numericos de ponto flutuante.
-		float numeroFlutuante = 6.2f;		// Armazena um numero flutuante de 32 bits, por padrao e 0.0f.
-		double numeroFlutuanteDouble = 4.6;	// Armazena um numero flutuante de 64 bits, por padrao e 0.0.
+		// Tipos primitivos numéricos de ponto flutuante.
+		float numeroFlutuante = 6.2f;		// Armazena um número flutuante de 32 bits, por padrão é 0.0f.
+		double numeroFlutuanteDouble = 4.6;	// Armazena um número flutuante de 64 bits, por padrão é 0.0.
 
 		System.out.println("Classe sobre comentários e tipos primitivos.");
 
